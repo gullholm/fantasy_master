58289f41c2965ba30d205f8ce76beee4d87466cd
Jonstal <60480280+Jonstal@users.noreply.github.com>
2022-01-25 10:58:09 +0100

    Create calculations.py

---
+++ calculations.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 25 10:55:46 2022
+
+@author: jonat
+"""
+
+import numpy as np
+
+def nump2(n, k):
+    a = np.ones((k, n-k+1), dtype=int)
+    a[0] = np.arange(n-k+1)
+    for j in range(1, k):
+        reps = (n-k+j) - a[j-1]
+        a = np.repeat(a, reps, axis=1)
+        ind = np.add.accumulate(reps)
+        a[j, ind[:-1]] = 1-reps[1:]
+        a[j, 0] = j
+        a[j] = np.add.accumulate(a[j])
+    return a
+
+def calcindex(indexlist, dat, nr, length):
+    returnlist=[]
+    for i in range(length):
+        temp = []
+        for j in range(nr):    
+            temp.append(list(dat)[indexlist[i][j]])
+        returnlist.append(temp)
+    return returnlist
+
+def pointsPerTeam3(team):
+    teampoints = 0
+    for key in team: 
+        teampoints = teampoints + players[key]["total_points"]
+     
+    return teampoints
+
+def pointsPerTeam4(team):
+    teampoints = 0
+    for key in team: 
+        teampoints = teampoints + pointsList[key-1]
+     
+    return teampoints
+
+
+def costPerTeam(team):
+    teamcost = 0
+    for key in team:
+        teamcost = teamcost + players[key]["now_cost"]
+    return teamcost  
+
+def costPerTeam4(team):
+    teamcost = 0
+    for key in team:
+        teamcost = teamcost + costList[key-1]
+    return teamcost  
+
+def createCostList():
+    costList = []
+    for i in range(len(players)):
+        costList.append(players[i+1]["now_cost"])
+    #for player in players:
+    #    costList.append(player["now_cost"])    
+    return costList
+
+def createPointsList():
+    pointsList=[]
+    for i in range(len(players)):
+        pointsList.append(players[i+1]["total_points"])
+    #for player in players:
+     #   pointsList.append(player["total_points"])
+    return pointsList
+
+def createFormation(d = 4, m = 4, f = 2, n = 100):
+    
+    defe = np.transpose(nump2(len(df),d))
+    midf = np.transpose(nump2(len(mf),m))
+    forw = np.transpose(nump2(len(fw),f))    
+        
+    forwards = calcindex(forw, fw, f, n) 
+    defenders = calcindex(defe, df, d, n )
+    midfielders = calcindex(midf, mf, m, n)
+    
+    return defenders, midfielders, forwards
+
+def printSummary(teamPoints, teamCosts):
+    
+    index_max = np.argmax(teamPoints)
+    meanCost = round(sum(teamCosts)/len(teamCosts)) 
+    meanPoints = round(sum(teamPoints)/len(teamPoints))
+
+    print("Nr of teams: " + str(len(teamPoints)))
+    print("Best index: " + str(index_max))
+    #print("Indexes for the best team: " + str(teams[index_max]))
+    print("Best score: "+ str(teamPoints[index_max]))
+    print("Total cost for the best team: " + str(teamCosts[index_max]))
+    print("Mean cost: " + str(meanCost))
+    print("Mean points: " + str(meanPoints))
+    
+    #return None
