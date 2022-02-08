b53911ffa0beed728118d9d36ed8eeb3c183675e
Jonstal <60480280+Jonstal@users.noreply.github.com>
2022-02-08 15:52:36 +0100

    change

--- calc_result_pl.py
+++ calc_result_pl.py
@@ -14,19 +14,13 @@ import calculations as calc
 import cleaners
 import parsers
 
-data2 = getters.get_data()
-players = getters.get_players_feature(data2)
-# gk, df, mf, fw = getters.get_diff_pos(players)
-# gk1, def4, mid4, forw2 = calc.createFormation(gk,df,mf,fw, 4, 4, 2, 100)
+# 2021
 
 playerspl = pd.read_csv("data/pl_csv/players_raw_2021.csv") 
-#playerspl = playerspl.set_index('id').T.to_dict()
 playerspl = playerspl.to_dict('index')
-#print(playerspl[1])
  
 
 playerspldata = getters.get_players_feature_pl(playerspl)
-
 
 formations = [[3,4,5],[3,4,5],[1,2,3]]
 form_name = ["df", "mf", "fw"]
@@ -40,7 +34,7 @@ for part, df, pos in zip(formations, all_parts_but_goalie, form_name):
     for p in part:
         all_cleaned = cleaners.run_all_cleans(df, p)
         combs = parsers.create_all_combs_from_cleaned_df(playerspldata, all_cleaned, p)[0]
-        combs.to_csv("data_cleaned/pl/" + pos + "/" + str(p) + ".csv")
-        combs.to_csv("data_cleaned/pl/" + pos + "/" + str(p) + ".csv",index = False)
+        combs.to_csv("data_cleaned/pl/2021" + pos + "/" + str(p) + ".csv")
+        combs.to_csv("data_cleaned/pl/2021" + pos + "/" + str(p) + ".csv",index = False)
 
 
