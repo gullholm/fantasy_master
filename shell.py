ee3589b1cb50fac6e04adaa88b44066755cf17a1
Josef <jgullholm@gmail.com>
2022-02-17 14:08:04 +0100

    blabla

--- shell.py
+++ shell.py
@@ -4,7 +4,7 @@ Created on Fri Jan 21 09:08:44 2022
 
 @author: jgull
 """
-
+import numpy as np
 import getters
 
 data = getters.get_data()
@@ -17,3 +17,15 @@ team = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 #ddd = getters.get_diff_pos(players)
 
 gk, d, mf, fwd = getters.get_diff_pos(players)
+
+team_names = getters.get_cost_team(data, team)
+
+
+ll= [1,2,3]
+
+ee = np.array(ll)
+
+kk = [10,15,20,35]
+ddd = np.array(kk)
+
+print(np.sum(ddd[ll]))
