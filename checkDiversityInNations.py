035a187d43e48081fcf9b5e9859f51d22344ec04
Jonstal <60480280+Jonstal@users.noreply.github.com>
2022-03-01 10:52:47 +0100

    a

--- checkDiversityInNations.py
+++ checkDiversityInNations.py
@@ -11,14 +11,23 @@ Created on Mon Feb 28 14:46:29 2022
 
 
 import pandas as pd
+import ast
 
 
 csv_file= "data/nationalities/pl/2021/players2021.csv" 
 nationalitiespl = pd.read_csv(csv_file)
 
-total_players = nationalitiespl[' Players'].sum()
+total_players = nationalitiespl['Players'].sum()
+nationalitiespl['Percentage']= [nationalitiespl['Players'][i]/total_players for i in range(len(nationalitiespl))]
 
-nationalitiespl['Percentage']= [nationalitiespl[' Players'][i]/total_players for i in range(len(nationalitiespl))]
-
-
-
+playerspernat= []
+for j in range(len(nationalitiespl)):
+    
+    nation = nationalitiespl['List'][1].split(" ")
+    playerspernat.append([nation[i] +' '+ nation[i+1] for i in range(0,int((len(nation)-1)/2))])
+    
+    
+ 
+    
+ 
+    
