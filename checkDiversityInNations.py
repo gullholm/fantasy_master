b412ca184059c16a233bfde58363d2b0ce2e7058
Jonstal <60480280+Jonstal@users.noreply.github.com>
2022-03-01 16:15:41 +0100

    nations

--- checkDiversityInNations.py
+++ checkDiversityInNations.py
@@ -12,38 +12,48 @@ Created on Mon Feb 28 14:46:29 2022
 
 import pandas as pd
 import ast
+from collections import Counter
 
 
-csv_file2 = "data/nationalities/pl/2021/players2021test.csv"  
-nationalitiespltest = pd.read_csv(csv_file2, sep='\t')
+csv_file2 = "data/nationalities/pl/nationalities.csv"  
+nationalitiespl = pd.read_csv(csv_file2, sep='\t')
 
-tot_play = [int(nationalitiespltest['Players'][j]) for j in range(len(nationalitiespltest))]
-total_players= sum(tot_play)
-nationalitiespltest['Percentage']= [nationalitiespltest['Players'][i]/total_players for i in range(len(nationalitiespltest))]
-
-plpernat = [nationalitiespltest['Names'][j].split(', ') for j in range(len(nationalitiespltest))] 
+plpernat = [nationalitiespl['Names'][j].split(', ') for j in range(len(nationalitiespl))] 
    
 def checkNationality(namelist, playerspernat):
-    
+    nations= []
     for name in namelist:
         for i, nat in enumerate(playerspernat): 
             for pl in nat:
                 if pl==name: 
-                    print(str(name) + ' is from ' +str(nationalitiespltest['Nation'][i]))
+                    #print(str(name) + ' is from ' +str(nationalitiespl['Nation'][i]))
+                    nations.append(nationalitiespl['Nation'][i])
                     break
-
+    return nations
 
 #%%
+import matplotlib.pyplot as plt
+seasons = [1617,1718,1819,1920,2021]
 
-results = pd.read_csv('results/pl/2021/''best.csv')
-names = [ast.literal_eval(results['Name'][i]) for i in range(len(results))]
+for season in seasons:
+    print(season)
+    csv_file = 'results/pl/' + str(season) +'/best.csv'
+    results = pd.read_csv(csv_file)
+    names = [ast.literal_eval(results['Name'][i]) for i in range(len(results))]
 
-for i in range(len(names)):
-    print(i)
-    checkNationality(names[i], plpernat)
-    
-    
+    for i in range(len(names)):
+        print('--------------------------------------')
+        nations = checkNationality(names[i], plpernat)
+        #print(len(nations))
+        print(Counter(nations))
+
  
+#%%
+csv_file = "data/nationalities/pl/2021.csv"  
+statsnations = pd.read_csv(csv_file)
+#tot_play = [int(nationalitiespltest['Players'][j]) for j in range(len(nationalitiespltest))]
+#total_players= sum(tot_play)
+#nationalitiespltest['Percentage']= [nationalitiespltest['Players'][i]/total_players for i in range(len(nationalitiespltest))]
     
  
     
